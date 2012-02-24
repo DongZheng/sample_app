8b4bff6d661d7d4db339b9a6726683bb2d26fba1
Zheng Dong <a23211@motorola.com>
2012-02-24 11:01:23 +0800

    Ch7.2.4

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -67,9 +67,9 @@ describe User do
           hash = @attr.merge(:password => long, :password_confirmation => long)
           User.new(hash).should_not be_valid
         end
-      end
+    end
       
-      describe "password encryption" do
+    describe "password encryption" do
 
           before(:each) do
             @user = User.create!(@attr)
@@ -78,5 +78,40 @@ describe User do
           it "should have an encrypted password attribute" do
             @user.should respond_to(:encrypted_password)
           end
-        end
+          
+          it "should set the encrypted password" do
+                @user.encrypted_password.should_not be_blank
+          end
+          
+          describe "has_password? method" do
+
+                it "should be true if the passwords match" do
+                  @user.has_password?(@attr[:password]).should be_true
+                end    
+
+                it "should be false if the passwords don't match" do
+                  @user.has_password?("invalid").should be_false
+                end 
+          end
+          
+          describe "authenticate method" do
+
+                it "should return nil on email/password mismatch" do
+                  wrong_password_user = User.authenticate(@attr[:email], "wrongpass")
+                  wrong_password_user.should be_nil
+                end
+
+                it "should return nil for an email address with no user" do
+                  nonexistent_user = User.authenticate("bar@foo.com", @attr[:password])
+                  nonexistent_user.should be_nil
+                end
+
+                it "should return the user on email/password match" do
+                  matching_user = User.authenticate(@attr[:email], @attr[:password])
+                  matching_user.should == @user
+                end
+          end
+    end
+        
+        
 end
